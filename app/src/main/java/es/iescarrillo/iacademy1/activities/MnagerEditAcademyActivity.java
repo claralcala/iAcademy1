180b6ac4f14b706e582b5245b7519f83e02bacc6
Clara Alcalá García <calcgar312@iescarrillo.es>
2023-11-03 18:27:28 +0100

    cambios - actividades agregadas

---
+++ app/src/main/java/es/iescarrillo/iacademy1/activities/MnagerEditAcademyActivity.java
@@ -0,0 +1,16 @@
+package es.iescarrillo.iacademy1.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import es.iescarrillo.iacademy1.R;
+
+public class MnagerEditAcademyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mnager_edit_academy);
+    }
+}
