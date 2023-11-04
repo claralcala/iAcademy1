8436a4a1e882d2e4b04d7ad3d7d1e4ed126cdeb1
Clara Alcalá García <calcgar312@iescarrillo.es>
2023-11-04 13:47:23 +0100

    Añadidas actividades y xml

---
+++ app/src/main/java/es/iescarrillo/iacademy1/activities/RegisterCourseActivity.java
@@ -0,0 +1,16 @@
+package es.iescarrillo.iacademy1.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import es.iescarrillo.iacademy1.R;
+
+public class RegisterCourseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_course);
+    }
+}
